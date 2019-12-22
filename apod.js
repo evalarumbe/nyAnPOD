864474233d083e1daa5d2945f8923040ac9a1bae
Eva Larumbe <evalarumbe@gmail.com>
2019-12-22 22:26:52 +0100

    Add background

--- apod.js
+++ apod.js
@@ -4,7 +4,11 @@
     
     todayString = [today.getUTCFullYear(), today.getMonth(), today.getDate()].join('-');
 
+    // why do i need await twice?
     const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${todayString}`);
+    const data = await response.json();
+    const { url, explanation } = data;
 
-    console.log(response);
+    const html = document.querySelector('html');
+    html.style.backgroundImage = `url(${url})`;
 })();
