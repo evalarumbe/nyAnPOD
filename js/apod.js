fc5b70914f120ca54eebd0e5cf6e5db76243e132
Eva Larumbe <evalarumbe@gmail.com>
2019-12-31 03:46:15 +0100

    Get APOD based on user date, yay! (Still ugly and needs more error handling)

--- js/apod.js
+++ js/apod.js
@@ -1,16 +1,21 @@
 (async () => {    
     const renderApod = async (date = new Date()) => {
         const html = document.querySelector('html');
-        dateString = [date.getUTCFullYear(), date.getMonth(), date.getDate()].join('-');
-    
-        // const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${dateString}`);
-        // const data = await response.json();
-        // const { url, title, explanation } = data;
+
+        try {
+            dateString = [date.getUTCFullYear(), date.getMonth() + 1, date.getDate()].join('-');
+        } catch(err) {
+            console.error(err);
+        }
+        
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${dateString}`);
+        const data = await response.json();
+        const { url, title, explanation } = data;
         
         // TODO: use above real data. This is a placeholder (too many requests)
-        const url = "https://apod.nasa.gov/apod/image/1707/M63-HST-Subaru-S1024.jpg";
-        const title = "Messier 63: The Sunflower Galaxy";
-        const explanation = "A bright spiral galaxy of the northern sky, Messier 63 is about 25 million light-years distant in the loyal constellation Canes Venatici. Also cataloged as NGC 5055, the majestic island universe is nearly 100,000 light-years across. That's about the size of our own Milky Way Galaxy. Known by the popular moniker, The Sunflower Galaxy, M63 sports a bright yellowish core in this sharp composite image from space- and ground-based telescopes. Its sweeping blue spiral arms are streaked with cosmic dust lanes and dotted with pink star forming regions. A dominant member of a known galaxy group, M63 has faint, extended features that are likely star streams from tidally disrupted satellite galaxies. M63 shines across the electromagnetic spectrum and is thought to have undergone bursts of intense star formation.";
+        // const url = "https://apod.nasa.gov/apod/image/1707/M63-HST-Subaru-S1024.jpg";
+        // const title = "Messier 63: The Sunflower Galaxy";
+        // const explanation = "A bright spiral galaxy of the northern sky, Messier 63 is about 25 million light-years distant in the loyal constellation Canes Venatici. Also cataloged as NGC 5055, the majestic island universe is nearly 100,000 light-years across. That's about the size of our own Milky Way Galaxy. Known by the popular moniker, The Sunflower Galaxy, M63 sports a bright yellowish core in this sharp composite image from space- and ground-based telescopes. Its sweeping blue spiral arms are streaked with cosmic dust lanes and dotted with pink star forming regions. A dominant member of a known galaxy group, M63 has faint, extended features that are likely star streams from tidally disrupted satellite galaxies. M63 shines across the electromagnetic spectrum and is thought to have undergone bursts of intense star formation.";
     
         // set the APOD background
         html.style.background = `#31aafe url(${url}) center center / cover no-repeat fixed`;
@@ -20,6 +25,15 @@
         h1.textContent = title;
         const p = document.querySelector('#info p');
         p.textContent = explanation;
+    };
+
+    const handleForm = function(event) {
+        event.preventDefault();
+        // TODO: check for Invalid date (e.g. no input)
+        // TODO: check permitted date ranges for APOD
+        const date = new Date(this.date.value);
+        console.log(date);
+        renderApod(date);
     };
 
     const setPopUp = () => {
@@ -40,6 +54,8 @@
 
         document.querySelectorAll('.toggle-info').forEach((btn) => {btn.addEventListener('click', togglePopUp)});
     };
+
+    document.querySelector('#apod-date').addEventListener('submit', handleForm);
 
     renderApod();
     setPopUp();
