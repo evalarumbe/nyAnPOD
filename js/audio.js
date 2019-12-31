186f6a36683a8402c13bbb4852eb6b6cd1ac6af3
Eva Larumbe <evalarumbe@gmail.com>
2019-12-31 01:32:11 +0100

    Add listener for audio toggle

---
+++ js/audio.js
@@ -0,0 +1,19 @@
+(() => {
+    const audio = document.querySelector('audio');
+    const audioToggle = document.querySelector('#audio-toggle');
+    
+    const toggleAudio = function() {
+        if (audio.paused) {
+            audio.play();
+            audioToggle.classList.add('audible');
+        } else if (audio.muted) {
+            audio.muted = false;
+            audioToggle.classList.add('audible');
+        } else {
+            audio.muted = true;
+            audioToggle.classList.remove('audible');
+        }
+    };
+
+    audioToggle.addEventListener('click', toggleAudio);
+})();
